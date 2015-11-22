64e4c3ce4372567c86cc9ab6ab282628712223a9
James Ramsay <git@jwr.vc>
2015-11-22 18:14:33 +1100

    Passing basic regression tests

--- src/inflate-stream.js
+++ src/inflate-stream.js
@@ -1,19 +1,18 @@
 import through2 from 'through2';
 import fs from 'fs';
 import _ from 'lodash';
+// import request from 'request';
 
-/*
-
-Input stream: Object
-- link (object, required)
-  - href (string, required)
-  - hrefType (enum, required)
-
-Output stream: Object
-- chunk (string, required)
-
-Input and output properties can be altered by providing options
-
+/**
+* Input stream: object
+* - link (object, required)
+*   - href (string, required)
+*   - hrefType (enum, required)
+*
+* Output stream: object
+* - chunk (string, required)
+*
+* Input and output properties can be altered by providing options
 */
 
 const defaultOptions = {
@@ -35,7 +34,12 @@ module.exports = function inflateStream(options) {
 
     switch (link.hrefType) {
     case 'file':
-      content = fs.readFileSync(link.href, 'utf8').replace(/\n$/, '');
+      try {
+        content = fs.readFileSync(link.href, 'utf8').replace(/\n$/, '');
+      } catch (err) {
+        console.error(`Error: File (${link.href}) not found.`);
+        content = null;
+      }
       break;
     case 'string':
       content = link.href;
@@ -44,7 +48,10 @@ module.exports = function inflateStream(options) {
       content = '';
     }
 
-    chunk[opt.output] = content;
+    if (content !== null) {
+      chunk[opt.output] = content;
+    }
+
     this.push(chunk);
     return cb();
   }
---
+++ test/transcludeStream.js
@@ -0,0 +1,48 @@
+import test from 'ava';
+import nock from 'nock';
+import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
+import through2 from 'through2'
+
+import hercule  from '../lib/hercule';
+import fixtures from './fixtures';
+
+
+let mock = nock("http://github.com").get("/size.md").reply(200, "big\n");
+
+_.forEach((fixtures.fixtures), function(fixture) {
+
+  test('should transclude ' + fixture.name, (t) => {
+    let outputString = '';
+    let input = fs.createReadStream(fixture.inputFile, {encoding: 'utf8'});
+    let output = through2.obj(function (chunk, enc, cb) {
+      this.push(chunk);
+      cb();
+    });
+
+    t.plan(1);
+
+    output.on('readable', function () {
+      var content = null;
+      while (content = this.read()) {
+        outputString += content;
+      }
+    });
+
+    output.on('end', function () {
+      t.same(outputString, fixture.expectedOutput);
+      t.end();
+    })
+
+    let options = {
+      relativePath: path.dirname(fixture.inputFile),
+      parents: [],
+      parentRefs: []
+    }
+
+    hercule.transcludeStream(input, null, options, output);
+
+  });
+
+});
