4dabc8118030027f004f6a2305d1d74a2b3a9ca5
James Ramsay <git@jwr.vc>
2015-11-22 18:03:21 +1100

    Inflate stream

---
+++ src/inflate-stream.js
@@ -0,0 +1,53 @@
+var through2 = require('through2');
+var fs = require('fs');
+var _ = require('lodash');
+
+/*
+
+Input stream: Object
+- link (object, required)
+  - href (string, required)
+  - hrefType (enum, required)
+
+Output stream: Object
+- chunk (string, required)
+
+Input and output properties can be altered by providing options
+
+*/
+
+var defaultOptions = {
+  input: 'link',
+  output: 'chunk'
+};
+
+module.exports = function inflateStream(options) {
+  var opt = _.merge({}, defaultOptions, options);
+
+  function transform(chunk, encoding, cb) {
+    var content;
+    var link = chunk[opt.input];
+
+    if (!link) {
+      this.push(chunk);
+      return cb();
+    }
+
+    switch(link.hrefType) {
+      case 'file':
+        content = fs.readFileSync(link.href, 'utf8').replace(/\n$/, '');
+        break;
+      case 'string':
+        content = link.href;
+        break;
+      default:
+        content = '';
+    }
+
+    chunk[opt.output] = content;
+    this.push(chunk);
+    return cb();
+  }
+
+  return through2.obj(transform);
+};
---
+++ test/inflate-stream.js
@@ -0,0 +1,134 @@
+import test  from 'ava';
+import InflateStream from '../src/inflate-stream';
+
+
+test('should handle no input', (t) => {
+
+  let testStream = new InflateStream();
+
+  testStream.on('readable', function() {
+    var chunk = null;
+    while (chunk = this.read()) {
+      t.fail();
+    }
+  });
+
+  testStream.on('end', function() {
+    t.pass();
+    t.end();
+  });
+
+  testStream.end();
+
+});
+
+
+test('should skip input without link', (t) => {
+  let input = {
+    chunk: 'The quick brown fox jumps over the lazy dog./n'
+  }
+
+  let testStream = new InflateStream();
+
+  testStream.on('readable', function() {
+    var chunk = null;
+    while (chunk = this.read()) {
+      t.notOk(chunk.link);
+    }
+  });
+
+  testStream.on('end', function() {
+    t.end();
+  });
+
+  testStream.write(input)
+  testStream.end();
+
+});
+
+
+test('should inflate input with file link', (t) => {
+  let input = {
+    chunk: ':[Example](size.md)',
+    link: {
+      href: __dirname + '/fixtures/basic/size.md',
+      hrefType: 'file'
+    }
+  }
+  let expected = 'big'
+
+  let testStream = new InflateStream();
+
+  testStream.on('readable', function() {
+    var chunk = null;
+    while (chunk = this.read()) {
+      t.same(chunk.chunk, expected);
+    }
+  });
+
+  testStream.on('end', function() {
+    t.end();
+  });
+
+  testStream.write(input)
+  testStream.end();
+
+});
+
+
+test('should inflate input with string link', (t) => {
+  let input = {
+    chunk: ':[Example](size.md)',
+    link: {
+      href: 'tiny',
+      hrefType: 'string'
+    }
+  }
+  let expected = 'tiny'
+
+  let testStream = new InflateStream();
+
+  testStream.on('readable', function() {
+    var chunk = null;
+    while (chunk = this.read()) {
+      t.same(chunk.chunk, expected);
+    }
+  });
+
+  testStream.on('end', function() {
+    t.end();
+  });
+
+  testStream.write(input)
+  testStream.end();
+
+});
+
+
+test('should return empty string when hrefType is not recognised', (t) => {
+  let input = {
+    chunk: ':[Example](size.md)',
+    link: {
+      href: 'http://example.com',
+      hrefType: 'http'
+    }
+  }
+  let expected = ''
+
+  let testStream = new InflateStream();
+
+  testStream.on('readable', function() {
+    var chunk = null;
+    while (chunk = this.read()) {
+      t.same(chunk.chunk, expected);
+    }
+  });
+
+  testStream.on('end', function() {
+    t.end();
+  });
+
+  testStream.write(input)
+  testStream.end();
+
+});
