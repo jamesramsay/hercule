f7a22442e20f2fb0145d97d0fcf6013036425a28
James Ramsay <git@jwr.vc>
2015-12-11 18:34:37 +1100

    chore(indent): improved code style

--- src/indent-stream.js
+++ src/indent-stream.js
@@ -2,11 +2,14 @@ import through2 from 'through2';
 import _ from 'lodash';
 
 /**
-* Indents each line of a chunk
+* Indents each line of a chunk by the provided indent amount
 *
 * Input stream: (object)
+* - content (string, required)
+* - indent (string, optional) - String to be appended to the start of each line.
 *
 * Output stream: (object)
+* - content (string)
 */
 
 const defaultOptions = {
@@ -20,15 +23,18 @@ module.exports = function IndentStream(options) {
 
   function transform(chunk, encoding, callback) {
     const indent = _.get(chunk, opt.indent);
+    let content = _.get(chunk, opt.input);
+    let output;
 
     if (!indent) {
       this.push(chunk);
       return callback();
     }
 
-    chunk[opt.output] = chunk[opt.input].replace(/\n/g, `\n${indent}`);
+    content = content.replace(/\n/g, `\n${indent}`);
+    output = _.assign(chunk, {[opt.output]: content});
 
-    this.push(chunk);
+    this.push(output);
     return callback();
   }
 
