530d42fca7efa109971156ad7789015d9c02a40a
James Ramsay <git@jwr.vc>
2015-12-31 15:02:21 +1100

    Build: run tests against multiple node versions (refs #134)
    - Skip sync API tests on versions prior to 0.12

--- test/integration/transcludeFileSync.js
+++ test/integration/transcludeFileSync.js
@@ -7,41 +7,52 @@ import bunyan from 'bunyan';
 import {transcludeFileSync} from '../../lib/hercule';
 import fixtures from '../fixtures';
 
+let major;
+let minor;
+let patch;
 
-test.beforeEach((t) => {
-  t.context.logOutput = [];
-  t.context.logStream = through2.obj();
+[major, minor, patch] = process.versions.node.split('.');
 
-  t.context.log = bunyan.createLogger({
-    name: 'hercule',
-    streams: [{
-      stream: t.context.logStream,
-    }],
+if (major == 0 && minor < 12) {
+  test(`synchronous support not available < 0.12`, (t) => {
+    t.pass();
   });
+} else {
+  test.beforeEach((t) => {
+    t.context.logOutput = [];
+    t.context.logStream = through2.obj();
 
-  t.context.logStream.on('readable', function read() {
-    let message = null;
-    while ((message = this.read()) !== null) {
-      message = _.pick(JSON.parse(message), 'name', 'msg', 'link', 'level');
+    t.context.log = bunyan.createLogger({
+      name: 'hercule',
+      streams: [{
+        stream: t.context.logStream,
+      }],
+    });
 
-      // Make paths relatice to process for testing purposes
-      if (message.link && message.link.href) {
-        message.link.href = path.relative(process.cwd(), message.link.href);
+    t.context.logStream.on('readable', function read() {
+      let message = null;
+      while ((message = this.read()) !== null) {
+        message = _.pick(JSON.parse(message), 'name', 'msg', 'link', 'level');
+
+        // Make paths relatice to process for testing purposes
+        if (message.link && message.link.href) {
+          message.link.href = path.relative(process.cwd(), message.link.href);
+        }
+
+        t.context.logOutput.push(message);
       }
-
-      t.context.logOutput.push(message);
-    }
+    });
   });
-});
 
 
-_.forEach((fixtures.fixtures), (fixture) => {
-  // Exclude http tests because mocking won't cover sync sub-process
-  if (_.contains(['http-link', 'http-deep-nesting'], fixture.name)) return;
+  _.forEach((fixtures.fixtures), (fixture) => {
+    // Exclude http tests because mocking won't cover sync sub-process
+    if (_.contains(['http-link', 'http-deep-nesting'], fixture.name)) return;
 
-  test(`should transclude ${fixture.name}`, (t) => {
-    const output = transcludeFileSync(fixture.inputFile, {}, t.context.log);
-    t.same(output, fixture.expectedOutput);
-    t.same(t.context.logOutput, fixture.expectedLogOutput);
+    test(`should transclude ${fixture.name}`, (t) => {
+      const output = transcludeFileSync(fixture.inputFile, {}, t.context.log);
+      t.same(output, fixture.expectedOutput);
+      t.same(t.context.logOutput, fixture.expectedLogOutput);
+    });
   });
-});
+}
