963663f2123567ea00e4b3b7b4d1202b81877f1a
James Ramsay <james@jramsay.com.au>
2016-03-09 13:44:31 +1100

    Docs: add gulp example and docs

---
+++ examples/gulp/gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var hercule = require('hercule');
+var through = require('through2');
+
+gulp.task('hercule', function() {
+  function gulpHercule(options) {
+    return through.obj( function(file, encoding, callback) {
+      if (file.isNull()) {
+        return callback(null, file);
+      }
+
+      if (file.isBuffer()) {
+        hercule.transcludeString(file.contents.toString(encoding), options, function(output) {
+          file.contents = new Buffer(output);
+          return callback(null, file);
+        })
+      }
+
+      if (file.isStream()) {
+        var transcluder = new hercule.TranscludeStream(options);
+        file.contents = file.contents.pipe(transcluder);
+        return callback(null, file);
+      }
+    });
+  };
+
+  gulp.src('api.apib', { buffer: false })
+    .pipe(gulpHercule())
+    .pipe(gulp.dest('build'));
+});
