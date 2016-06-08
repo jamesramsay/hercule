6fc311c6b71e2af27ebfc96cd1a88b2c33c9bfc6
James Ramsay <git@jwr.vc>
2016-06-09 07:37:53 +1000

    Fix: sync unit tests depending on pre-compiled code

--- test/units/transcludeFileSync.js
+++ test/units/transcludeFileSync.js
@@ -1,5 +1,6 @@
 import test from 'ava';
-import path from 'path';
+import sinon from 'sinon';
+global.childProcess = require('child_process');
 
 import { transcludeFileSync } from '../../src/hercule';
 
@@ -12,24 +13,37 @@ if (major < 1 && minor < 12) {
   });
 }
 
+test.before(() => {
+  const stub = sinon.stub(global.childProcess, 'spawnSync');
+  stub.withArgs('../bin/hercule', ['/index.md', '--reporter', 'json-err'])
+    .returns({ stdout: 'The quick brown fox jumps over the lazy dog.\n', stderr: '' });
+
+  stub.withArgs('../bin/hercule', ['/index.md', '--reporter', 'json-err', '--relativePath', 'test'])
+    .returns({ stdout: 'Jackdaws love my big sphinx of quartz.\n', stderr: '' });
+
+  stub.withArgs('../bin/hercule', ['/error.md', '--reporter', 'json-err'])
+    .returns({ stdout: '', stderr: 'ERROR' });
+});
+
+test.after(() => {
+  global.childProcess.spawnSync.restore();
+});
+
 test('should transclude with only required arguments', (t) => {
-  const input = path.join(__dirname, '../fixtures/no-link/index.md');
   const expected = 'The quick brown fox jumps over the lazy dog.\n';
-  const output = transcludeFileSync(input);
+  const output = transcludeFileSync('/index.md');
   t.deepEqual(output, expected);
 });
 
 test('should transclude with optional relativePath argument', (t) => {
-  const input = path.join(__dirname, '../fixtures/no-link/index.md');
-  const expected = 'The quick brown fox jumps over the lazy dog.\n';
-  const output = transcludeFileSync(input, { relativePath: 'test' });
+  const expected = 'Jackdaws love my big sphinx of quartz.\n';
+  const output = transcludeFileSync('/index.md', { relativePath: 'test' });
   t.deepEqual(output, expected);
 });
 
 test('should throw error with invalid link', (t) => {
-  const input = path.join(__dirname, '../fixtures/invalid-link/index.md');
   try {
-    transcludeFileSync(input);
+    transcludeFileSync('/error.md');
     t.fail();
   } catch (ex) {
     t.deepEqual(ex.message, 'Could not transclude file');
